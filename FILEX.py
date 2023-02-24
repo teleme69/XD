31028064c9d6cb875513f9cef074f45336e5dd04
Ahmed-XD <110410370+Ahmed-XD@users.noreply.github.com>
2023-02-24 06:29:07 +0600

    Update FILEX.py

--- FILEX.py
+++ FILEX.py
@@ -2,6 +2,10 @@ import os
 print('give star to our repo')
 os.system('git pull && clear')
 
-print('update finished')
+print('update finished\nJoin Our Facebook Group and support us')
+os.system('xdg-open https://m.facebook.com/groups/1247184652736578/')
+
+
 os.system('clear')
+
 import XXV
