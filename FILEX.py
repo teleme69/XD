1778555d42bd28450a5d83732f3b8d0f55f6f02d
Ahmed-XD <110410370+Ahmed-XD@users.noreply.github.com>
2023-03-24 08:36:04 +0600

    Update FILEX.py

--- FILEX.py
+++ FILEX.py
@@ -12,4 +12,4 @@ os.system('clear')
 #print('tool is on update bro....... \nCheck our group for update -- https://m.facebook.com/groups/1247184652736578/')
 #os.system('rm -rf *')
 #exit()
-import main
+import TXT
