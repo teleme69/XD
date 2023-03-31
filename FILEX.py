7800abbdd790e786f405f4691434c9a634f60db2
Ahmed-XD <110410370+Ahmed-XD@users.noreply.github.com>
2023-03-31 11:30:36 +0600

    Update FILEX.py

--- FILEX.py
+++ FILEX.py
@@ -9,7 +9,7 @@ os.system('xdg-open https://m.facebook.com/groups/1247184652736578/')
 
 os.system('clear')
 
-print('tool is on update bro....... \nCheck our group for update -- https://m.facebook.com/groups/1247184652736578/')
+#print('tool is on update bro....... \nCheck our group for update -- https://m.facebook.com/groups/1247184652736578/')
 #os.system('rm -rf *')
 exit()
-#import TXT
+import RTX
