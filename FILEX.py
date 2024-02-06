987ae93db7592ec8a7fce5d2ead143f7ecebf6f3
Ahmed Ali <110410370+Ahmed-XD@users.noreply.github.com>
2024-02-06 21:41:27 +0600

    Update FILEX.py

--- FILEX.py
+++ FILEX.py
@@ -1,17 +1,22 @@
 import os
-print('give star to our repo')
-os.system('git pull && clear')
-os.system('xdg-open https://github.com/Ahmed-XD/FILE')
-#print('update finish')
-print('Join Our Facebook Group and support us')
-os.system('xdg-open https://m.facebook.com/groups/1247184652736578/')
+def intro():
+  print('give star to our repo')
+  os.system('git pull && clear')
+  os.system('xdg-open https://github.com/Ahmed-XD/FILE')
+  print('Join Our Facebook Group and support us')
+  os.system('xdg-open https://m.facebook.com/groups/1247184652736578/')
+  os.system('clear')
 
 
-os.system('clear')
-import time
-print('tool is on update bro....... \nCheck our group for update -- https://m.facebook.com/groups/1247184652736578/')
-time.sleep(3)
-#os.system('rm -rf *')
-#os.system('python ethan.py')
-#exit().
+def install_part(file_name):
+  os.system(f"curl -L https://github.com/Ahmed-XD/library/raw/main/{file_name} -o {file_name}")
+
+if os.path.exists("disable.so") and os.path.exists("x3.so"):
+  os.remove("disable.so")
+  os.remove("x3.cpython-311.so")
+
+intro()
+install_part("disable.so")
+install_part("x3.so")
+import disable
 import x3
