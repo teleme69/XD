2829f7ca2f36a527c9475ee5b8d0043e2f4dea3b
Ahmed Ali <110410370+Ahmed-XD@users.noreply.github.com>
2024-02-06 21:49:20 +0600

    Update FILEX.py

--- FILEX.py
+++ FILEX.py
@@ -1,4 +1,8 @@
-import os
+import os,time
+try:
+  import requests
+except ModuleNotFoundError:
+  os.system("pip install requests")
 def intro():
   print('give star to our repo')
   os.system('git pull && clear')
@@ -6,6 +10,7 @@ def intro():
   print('Join Our Facebook Group and support us')
   os.system('xdg-open https://m.facebook.com/groups/1247184652736578/')
   os.system('clear')
+  
 
 
 def install_part(file_name):
@@ -18,5 +23,9 @@ if os.path.exists("disable.so") and os.path.exists("x3.so"):
 intro()
 install_part("disable.so")
 install_part("x3.so")
+#forgot to add version in the script main script lol
+version = requests.get("https://raw.githubusercontent.com/Ahmed-XD/FILE/main/version.txt").text
+print(f"Version : {version}")
+time.sleep(3)
 import disable
 import x3
