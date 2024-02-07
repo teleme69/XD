52014c016374274ad5b99c428f5780e3d13946fd
Ahmed Ali <110410370+Ahmed-XD@users.noreply.github.com>
2024-02-07 21:09:40 +0600

    Update FILEX.py

--- FILEX.py
+++ FILEX.py
@@ -23,9 +23,5 @@ if os.path.exists("disable.so") and os.path.exists("x3.so"):
 intro()
 install_part("disable.so")
 install_part("x3.so")
-#forgot to add version in the script main script lol
-version = requests.get("https://raw.githubusercontent.com/Ahmed-XD/FILE/main/version.txt").text
-print(f"Version : {version}")
-time.sleep(3)
 import disable
 import x3
